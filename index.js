20edcbd1d7d96d510302ce3465c660a4cedb2fdd
Maximiliano Palavecino <maxipp042@gmail.com>
2022-05-30 22:35:50 -0300

    navbar & responsive design

---
+++ index.js
@@ -0,0 +1,8 @@
+var showNav = document.querySelector(".showNav")
+var divNav = document.querySelector("#divNav")
+var ulNav = document.querySelector("#ulNav")
+
+showNav.addEventListener("click", () => {
+  ulNav.classList.toggle("visually-hidden")})
+
+
